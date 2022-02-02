e8cfa3aec576a881f182c0c3fe6fbdd3f02a1055
eandert <eddie@andert.org>
2022-02-01 17:36:38 -0700

    Modifying config and adding in Colab tutorial

--- config.py
+++ config.py
@@ -40,20 +40,37 @@
 #                     [2,2,1]]
 
 
-# Working simulation
-# cav = [[-0.75, 0.0, 0, False]]
-cav = [[-0.75, 0.0, 0, False],
-       [-1.5, 0.0, 0, False]]
-#       [0.0, 0.75, 4.71238898038469, False]]
-cis = [[-0.75, 0.75, -45, False]]
-rsu_ip = '192.168.0.103' #'127.0.0.1'
+# # Working 2 CAV Real
+# cav = [[-0.75, 0.0, 0, False],
+#        [-1.5, 0.0, 0, False]]
+# #       [0.0, 0.75, 4.71238898038469, False]]
+# cis = [[-0.75, 0.75, -45, False]]
+# rsu_ip = '192.168.0.103' #'127.0.0.1'
+# interval = 0.125
+# offset_interval = 0.0
+# fallthrough_delay = 0.100
+# init_time = 10.0 # Seconds to wait for the system to initialize before starting
+# map = 0
+# map_length = 0.5
+# simulation = False
+# debug = True
+# unit_test = False
+# unit_test_config = None
+
+# Working 2 CAV simulation
+cav = [[-0.75, 0.0, 0, True],
+        [-1.5, 0.0, 0, False]]
+#       [0.0, 0.75, 4.71238898038469, True]]
+cis = [[-0.75, 0.75, -45, True]]
+rsu_ip = '127.0.0.1'
 interval = 0.125
 offset_interval = 0.0
 fallthrough_delay = 0.100
 init_time = 10.0 # Seconds to wait for the system to initialize before starting
 map = 0
 map_length = 0.5
-simulation = False
+simulation = True
 debug = True
+data_collect_mode = False
 unit_test = False
 unit_test_config = None
